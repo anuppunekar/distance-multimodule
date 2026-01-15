4f3c2b9811c8700618e064e0c458d0b8153ee44f
anuppunekar <anup.punekar@gmail.com>
2026-01-16 00:54:06 +0530

    Created

--- distance-webapp/src/main/java/com/mydistance/DistanceServlet.java
+++ distance-webapp/src/main/java/com/mydistance/DistanceServlet.java
@@ -18,10 +18,14 @@ package com.mydistance;
 import com.mydistance.util.ConversionUtil;
 import java.io.IOException;
 import java.io.PrintWriter;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+//import javax.servlet.ServletException;
+//import javax.servlet.http.HttpServlet;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.apache.log4j.Logger;
 
 /**
